3eb308fd6b606d25d22991a99a1fd28d90932d94
Adan Figueroa <fija90@gmail.com>
2019-05-27 16:57:56 -0500

    Conectar webhook
    Se creó la ruta que permite conectar el webhook

--- app.js
+++ app.js
@@ -9,7 +9,21 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.set('port', process.env.PORT);
 
 app.get('/', (req,res)=>{
-    res.send('Hola este es mi servidor funcionando');
+    res.send('Hola comunidad DevC este es mi servidor funcionando');
+});
+
+app.get("/webhook", (req, res) => {
+    const mode = req.query['hub.mode'];
+    const challenge = req.query['hub.challenge'];
+    const token = req.query['hub.verify_token'];
+    if (mode && token) {
+      if (mode === "subscribe" && token === process.env.VERIFY_TOKEN) {
+        console.log("webhook listo");
+        res.status(200).send(challenge);
+      } else {
+        res.sendStatus(403);
+      }
+    }
 });
 
 app.listen(app.get('port'), () => {
