db02e1f7e15781d9beb53f95cbaf4738d6543f5a
Adan Figueroa <fija90@gmail.com>
2019-05-28 12:27:05 -0500

    Petición a Graph API para obtener el Public Profile
    Se realizó el método que permite consultar la información básica del usuario

--- services/graphAPI.js
+++ services/graphAPI.js
@@ -12,11 +12,32 @@ exports.callSendAPI = (requestBody) =>{
             method:"POST",
             json: requestBody
         },(error, body) =>{
+            // if(!error){
+            //     console.log("Petición envida", body);
+            // }else{
+            //     console.error("No se puedo realizar la petición",error);
+            // }
+        }
+    );
+}
+
+exports.getProfile = (senderID) =>{
+    const url = `https://graph.facebook.com/v3.3/${senderID}`;
+    request(
+        {
+            uri:url,
+            qs:{
+                access_token:process.env.ACCESS_TOKEN,
+                fields: "first_name,last_name, gender, locale, timezone"
+            },
+            method:"GET",
+        },(error, _res, body) =>{
             if(!error){
-                console.log("Petición envida", body);
-            }else{
-                console.error("No se puedo realizar la petición",error);
+                let response = JSON.parse(body);
+                console.log(`Nombre: ${response.first_name} Apellido: ${response.last_name}`);
             }
         }
     );
 }
+
+
