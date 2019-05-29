6098a7eeec1604e6a1a8df421b18c7ce8c98f8c5
Adan Figueroa <fija90@gmail.com>
2019-05-29 15:12:33 -0500

    Encuesta quick replies
    Simula una encuesta usando quick replies

--- services/actions.js
+++ services/actions.js
@@ -1,5 +1,40 @@
 const sendAPI = require("./graphAPI");
 
+const repliesSurvey ={
+    texto:'Por favor llena esta encuesta y dime ¿Qué es lo que más te gusta de nuestro servicio?',
+    replies:[
+        {
+            content_type:"text",
+            title:'Servicios',
+            payload:'servicio'
+        },
+        {
+            content_type:"text",
+            title:'Rapidez',
+            payload:'rapidez'
+        },
+        {
+            content_type:"text",
+            title:'Ubicación',
+            payload:'ubicacion'
+        }
+    ]
+}
+exports.quickReplies =( webhookEvent, replies)=>{
+    if(!replies){
+        replies = repliesSurvey;
+    }
+    let response = {
+        recipient: {
+            id: webhookEvent.sender.id
+        },
+        message:{
+            text:replies.texto,
+            quick_replies:replies.replies
+        }
+    }
+    sendAPI.callSendAPI(response);
+}
 exports.sendTextMessage = (texto, webhookEvent)=>{
     let response = {
         recipient: {
--- services/handleMessages.js
+++ services/handleMessages.js
@@ -1,19 +1,57 @@
 const actions = require("./actions");
 
+
 exports.handleMessage = (webhookEvent) =>{
     if(webhookEvent.message){
         let mensaje = webhookEvent.message;
         if(mensaje.quick_reply){
-            console.log("He recibido una respuesta rápida");
-        }else if(mensaje.attachment){
-            console.log("He recibido un mensaje con un elemento adjunto");
+            handleQuickReplies(webhookEvent);
+        }else if(mensaje.attachements){
+            console.log("Envió un adjunto");
         }else if(mensaje.text){
-            console.log("He recibido un mensaje de texto");
-            actions.sendTextMessage("Prueba de mensaje de texto",webhookEvent);
-        }else if(mensaje.postback){
-            console.log("He recibido un mensaje con postback");
+            console.log("Envió un texto");
+            actions.sendTextMessage("Has enviado texto", webhookEvent);
         }
+    }else if(webhookEvent.postback){
+        handlePostback(webhookEvent);
+    }
+}
+handlePostback = (webhookEvent) =>{
+    let evento = webhookEvent.postback.payload;
+    switch(evento){
+        case 'encuestas':
+            actions.quickReplies(webhookEvent);
+        break;
+        case 'sucursales':
+            actions.sendTextMessage("Oprimiste sucursales",webhookEvent);
+        break;
+        case 'inicio':
+            actions.sendTextMessage("Bienvenido al chatbot de DevC", webhookEvent);
+        break;        
+    }
+}
+
+
+handleQuickReplies = (webhookEvent) =>{
+    let reply = webhookEvent.message.quick_reply.payload;
+    const response = {
+        texto:"¿Nos recomendarías?",
+        replies:[
+        {
+            content_type:'text',
+            title:'Si',
+            payload:"siRecomienda"
+        },
+        {
+            content_type:'text',
+            title:'No',
+            payload:"noRecomienda"
+        }
+        ]
+    }
+    if(reply == 'rapidez' || reply == 'ubicacion' || reply == 'servicio'){
+        actions.quickReplies(webhookEvent,response);
     }else{
-        console.log("No se ha recibido un mensaje valido");
+        actions.sendTextMessage("Gracias por ayudarnos a mejorar",webhookEvent);
     }
 }
