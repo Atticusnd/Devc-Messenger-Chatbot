07c78214183cde7d74e1fa23fc6a1e76bad0df42
Adan Figueroa <fija90@gmail.com>
2019-05-29 15:06:02 -0500

    Manejar respuestas desde payloads
    Método que permite manejar llamadas que son hechas desde el menú persistente

--- services/handleMessages.js
+++ services/handleMessages.js
@@ -1,19 +1,34 @@
 const actions = require("./actions");
 
+
 exports.handleMessage = (webhookEvent) =>{
     if(webhookEvent.message){
         let mensaje = webhookEvent.message;
         if(mensaje.quick_reply){
-            console.log("He recibido una respuesta rápida");
-        }else if(mensaje.attachment){
-            console.log("He recibido un mensaje con un elemento adjunto");
+            console.log("Envió respuesta rápida");
+        }else if(mensaje.attachements){
+            console.log("Envió un adjunto");
         }else if(mensaje.text){
-            console.log("He recibido un mensaje de texto");
-            actions.sendTextMessage("Prueba de mensaje de texto",webhookEvent);
-        }else if(mensaje.postback){
-            console.log("He recibido un mensaje con postback");
+            console.log("Envió un texto");
+            actions.sendTextMessage("Has enviado texto", webhookEvent);
         }
-    }else{
-        console.log("No se ha recibido un mensaje valido");
+    }else if(webhookEvent.postback){
+        handlePostback(webhookEvent);
     }
 }
+handlePostback = (webhookEvent) =>{
+    let evento = webhookEvent.postback.payload;
+    switch(evento){
+        case 'encuestas':
+            console.log("Oprimió encuestas");
+        break;
+        case 'sucursales':
+            actions.sendTextMessage("Oprimiste sucursales",webhookEvent);
+        break;
+        case 'inicio':
+            actions.sendTextMessage("Bienvenido al chatbot de DevC", webhookEvent);
+        break;        
+    }
+}
+
+
